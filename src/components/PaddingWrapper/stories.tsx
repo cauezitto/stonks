0fa89111c0feb9674a6b7198191ade13dec0fcb4
cauezitto <caue.matheus.granrodeo@gmail>
2021-08-17 00:28:53 -0300

    removendo stories

--- src/components/PaddingWrapper/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import PaddingWrapper from '.'
-
-export default {
-  title: 'PaddingWrapper',
-  component: PaddingWrapper
-} as Meta
-
-export const Default: Story = () => <PaddingWrapper />
