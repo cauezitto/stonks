705e335d9226ff8a7c02ca4a4b86cc036964f6a0
cauezitto <caue.matheus.granrodeo@gmail>
2021-08-17 23:13:33 -0300

    iniciando seção dos clientes

--- src/components/Retangle/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Retangle from '.'
-
-export default {
-  title: 'Retangle',
-  component: Retangle
-} as Meta
-
-export const Default: Story = () => <Retangle />
