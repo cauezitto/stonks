a5e96cc26899be2ba994cded8603d7589f9ba757
cauezitto <caue.matheus.granrodeo@gmail>
2021-08-22 23:59:43 -0300

    finalizando base das sessões

--- src/components/Customer/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Customer from '.'
-
-export default {
-  title: 'Customer',
-  component: Customer
-} as Meta
-
-export const Default: Story = () => <Customer />
--- src/components/History/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import History from '.'
-
-export default {
-  title: 'History',
-  component: History
-} as Meta
-
-export const Default: Story = () => <History />
